848d6a408db1a7cf21cd0900a1c043130f250354
Valters Valdersteins <valters.valdersteins@gmail.com>
2016-12-23 20:06:05 +0200

    Initial repository content added

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './components/app/app.component';
+
+@NgModule({
+    providers: [
+
+    ],
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+
+export class AppModule { }
---
+++ src/components/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'ng2-cms',
+    template: '<h1>My App</h1>'
+})
+
+export class AppComponent { }
